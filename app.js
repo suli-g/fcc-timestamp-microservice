531c4ae41d697042c9e40d2e5e03545ea3f260b6
System <none>
2017-06-20 06:56:41 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const url = require('url');
 app.get('/:time', (req, res)=>{
-  res.send(req.params.id);
+  if (!req.params.time) res.write(`<h2>enter a date in unix or a natural format as a parameter in the url</h2> <blockquote> <h3>For example:</h3><p>${req.originalUrl}/20 March 2015</p></blockquote>`)
+  res.set('charset', 'utf8');
+  res.write(req.params.time);
   res.end();
 });
 app.listen(process.env.PORT);

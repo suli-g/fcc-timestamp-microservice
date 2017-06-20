f751c14e4a621136503e9aa144aa8ecc2a866703
System <none>
2017-06-20 12:47:26 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+'use strict';
 const express = require('express');
 const app = express();
 const url = require('url');
 app.get('', (req, res)=>{
    res.set('charset', 'utf8');
-  res.send("<h2>enter a date in either unix or a natural format as a parameter in the url</h2>");
-  res.send(`<blockquote> <h3>Examples:</h3><p>${req.hostname}/20 March 2015</p></blockquote>`);
+  let msg = "<h2>enter a date in either unix or a natural format as a parameter in the url</h2>";
+  let directions = `<p>unix: ${req.hostname}/123232131</p>`;
+  directions += `<p>natural: ${req.hostname}/30 June 2013</p>`;
+  directions += "<p>if natural dates aren't working as they should, replace the spaces with %20:<br>";
+  directions += `${req.hostname}/30%20June%202013</p>`
+  msg += `<blockquote> <h3>Examples:</h3>${directions}</blockquote>`;
+  res.send(msg);
   res.end();
 })
 app.get('/:time', (req, res)=>{

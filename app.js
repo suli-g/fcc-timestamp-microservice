c39b3cdc7b7caca9f261769c45bd8d6b4572066c
System <none>
2017-06-20 07:18:41 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const url = require('url');
 app.get('', (req, res)=>{
    res.set('charset', 'utf8');
-  res.write(`<h2>enter a date in unix or a natural format as a parameter in the url</h2> <blockquote> <h3>For example:</h3><p>${req.hostname}/20 March 2015</p></blockquote>`);
+  res.send("<h2>enter a date in either unix or a natural format as a parameter in the url</h2>");
+  res.send(`<blockquote> <h3>Examples:</h3><p>${req.hostname}/20 March 2015</p></blockquote>`);
   res.end();
 })
 app.get('/:time', (req, res)=>{

48b2dcd68e1faa84b0b1ba0ec9c41da4a8acf9eb
System <none>
2017-06-20 00:35:42 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const url = require('url');
 const moment = require('moment');
-app.set('')
-app.use(url.middleware(process.argv[0]));
 app.get('/', (req, res)=>{
-  re
-  res.write(process.argv[1]);
+  res.set({
+        'content-type': 'application/json',
+        'charset': 'utf8'
+    })
+  res.write(JSON.stringify({"url": process.argv[0]}));
   res.end();
 });
 app.listen(process.env.PORT);

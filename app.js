cd8756b5ddec8cffbd28409432a7c9a3f4936626
System <none>
2017-06-20 00:02:41 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("testing...");
+const http = require('express');
+const app = express();
+app.createServer((req, res)=>{
+  console.log(req);
+  res.end("!");
+});
+http.listen(process.env.PORT);

2641ea90ecc7c4ab7e92c7cec2d3be6e8d156737
System <none>
2017-06-20 00:13:41 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const http = require('express');
+const express = require('express');
 const app = express();
-app.createServer((req, res)=>{
-  console.log(req);
-  res.end("!");
+const url = require('url');
+const moment = require('moment');
+
+app.get((req, res)=>{
+  res.writeHead(200, {'Content-type':'application/json'});
+  let url_ = url.parse(req.url);
+  let time = moment(url_.search.substr(5));
+  let pathname = url_.pathname.substr(1);
+  res.write(time);
+  res.end();
 });
-http.listen(process.env.PORT);
+app.listen(process.argv[2], (err)=> {if(err) console.log(err)});

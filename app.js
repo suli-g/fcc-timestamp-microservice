8435c439c60975e26df480440d894e08523c26b1
System <none>
2017-06-21 08:00:49 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const moment = require('moment');
 const app = express();
+
 //the default state (if a user doesn't provide a date):
 app.get('', (req, res)=>{
+  
   res.charset = 'utf8';//set the encoding
   //the message to show:
+  
   let host = req.hostname;/*https://keen-sprite.glitch.me*/
   let msg = "<h2>enter a date in either unix or a natural format as a parameter in the url</h2>";
   let directions = `<p>for unix: ${host}/123232131</p><p>natural: ${host}/30 June 2013</p>`;
@@ -16,8 +19,10 @@ app.get('', (req, res)=>{
   //send the message
   res.send(msg);
   res.end();
-})
+});
+
 app.get('/:time', (req, res)=>{
+  
   //make sure the response format will be a JSON object
   res.set('content-type', 'application/json');
   //store the time provided in the url as a variable
@@ -25,6 +30,7 @@ app.get('/:time', (req, res)=>{
   //declare variables (use 'let' because they change)
   let unix, natural, date;
   //if the entry is an integer (2321313212131) and *only* an integer (not a2323213):
+  
   if (parseInt(entry) == entry){
     //store it as a unix date
     unix = moment.unix(entry).format("x");
@@ -32,18 +38,20 @@ app.get('/:time', (req, res)=>{
     natural = moment(entry, "x").format("MMMM DD, YYYY");
   }
   //if the date is *not* a unix date (basically, if its not an integer (even a1123)):
+  
   else {
     //store it as a natural date (still even a1123)
-    natural = moment(entry).format("MMMM DD, YYYY");
+    natural = moment(entry).format("MMMM D, YYYY");
     //convert the natural date to a unix date
-    unix = moment(entry).format("x");
+    unix = moment(natural).format("x");
   }
+  
   //if the date is not valid (like that a1123)
   if (!moment(natural).isValid() || !moment(unix, "x").isValid()){
     //set the date to null
     unix=null, natural=null;
   }
-  //store it as a JSON Object
+  //store the date as a JSON Object
   date = {"unix": unix, "natural": natural}
   //send the date JSON Object
   res.send(date);

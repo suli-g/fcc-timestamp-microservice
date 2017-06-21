9ea3ee94e1b2dab8cafc7c15904780ddc681ab0d
System <none>
2017-06-21 07:38:49 +0000

    Checkpoint

--- app.js
+++ app.js
@@ -18,26 +18,36 @@ app.get('', (req, res)=>{
   res.end();
 })
 app.get('/:time', (req, res)=>{
-  //make sure the response format will be a json object
+  //make sure the response format will be a JSON object
   res.set('content-type', 'application/json');
   //store the time provided in the url as a variable
   const entry = req.params.time;
   //declare variables (use 'let' because they change)
-  let unix, normal, date;
-  //if the entry is an integer (2321313212131) and *only* an integer (not a232321) 
+  let unix, natural, date;
+  //if the entry is an integer (2321313212131) and *only* an integer (not a2323213):
   if (parseInt(entry) == entry){
+    //store it as a unix date
     unix = moment.unix(entry).format("x");
-    normal = moment(entry, "x").format("MMMM DD, YYYY");
+    //convert the unix date to a natural language date
+    natural = moment(entry, "x").format("MMMM DD, YYYY");
   }
+  //if the date is *not* a unix date (basically, if its not an integer (even a1123)):
   else {
-    normal = moment(entry).format("MMMM DD, YYYY");
+    //store it as a natural date (still even a1123)
+    natural = moment(entry).format("MMMM DD, YYYY");
+    //convert the natural date to a unix date
     unix = moment(entry).format("x");
   }
-  if (!moment(normal).isValid() || !moment(unix, "x").isValid()){
-    unix=null, normal=null;
+  //if the date is not valid (like that a1123)
+  if (!moment(natural).isValid() || !moment(unix, "x").isValid()){
+    //set the date to null
+    unix=null, natural=null;
   }
-  date = {"unix": unix, "normal": normal}
+  //store it as a JSON Object
+  date = {"unix": unix, "natural": natural}
+  //send the date JSON Object
   res.send(date);
   res.end();
 });
+//listen to the port being used
 app.listen(process.env.PORT);
